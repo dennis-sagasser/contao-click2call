10eeb256063374871e92424852a3944e6f76ab7d
Dennis Sagasser <dennis.sagasser@gmail.com>
2014-12-29 11:39:13 +0100

    next commit

--- system/modules/click2call/config/config.php
+++ system/modules/click2call/config/config.php
@@ -1,31 +1,33 @@
 <?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
 
-/**
- * Contao Open Source CMS
- * Copyright (C) 2005-2011 Leo Feyer
+/**                                                                                                                                                                                                                                   
+ * Contao Open Source CMS                                                                                                                                                                                                             
+ * Copyright (C) 2005-2014 Leo Feyer                                                                                                                                                                                                  
+ *                                                                                                                                                                                                                                    
+ * Formerly known as TYPOlight Open Source CMS.                                                                                                                                                                                       
+ *                                                                                                                                                                                                                                    
+ * This program is free software: you can redistribute it and/or                                                                                                                                                                      
+ * modify it under the terms of the GNU Lesser General Public                                                                                                                                                                         
+ * License as published by the Free Software Foundation, either                                                                                                                                                                       
+ * version 3 of the License, or (at your option) any later version.                                                                                                                                                                   
+ *                                                                                                                                                                                                                                    
+ * This program is distributed in the hope that it will be useful,                                                                                                                                                                    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                                                                                                                     
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                                                                                                                                   
+ * Lesser General Public License for more details.                                                                                                                                                                                    
+ *                                                                                                                                                                                                                                    
+ * You should have received a copy of the GNU Lesser General Public                                                                                                                                                                   
+ * License along with this program. If not, please visit the Free                                                                                                                                                                     
+ * Software Foundation website at <http://www.gnu.org/licenses/>.                                                                                                                                                                     
+ *                                                                                                                                                                                                                                    
+ * PHP version 5     
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Dennis Sagasser 2013 
- * @author     Dennis Sagasser <sagasser@gispack.com> 
- * @package    CallbackService 
- * @license    GNU/LGPL 
- * @filesource
+ * @category  Contao  
+ * @package   CallbackSevice                                                                                                                                                                                                                                                                                                                                                                                                               
+ * @author    Dennis Sagasser <sagasser@gispack.com>
+ * @copyright 2014 Dennis Sagasser                                                                                                                                                                                                      
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ * @link      https://contao.org                                                                                                                                                                                                                                                                                                                                                                                                                                  
  */
 
 
